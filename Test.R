3ea730017bde6897e7d64c9b0255fca198f029d9
Stuart K Watson <swatso88@gmail.com>
2017-04-20 11:04:33 +0100

    basic

---
+++ Test.R
@@ -0,0 +1,10 @@
+#Test
+# ~ = working directory
+c<-as.matrix(read.table("~/Meta-project"))
+c<-as.matrix(read.table("~/Meta-project/collapsable.txt"))
+
+d<-as.data.frame(c)
+
+wd
+getwd()
+setwd(~)
